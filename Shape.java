57cd427076e15753f3dba59d9da1fb890bf9c186
Aman <84791435+Aman28801@users.noreply.github.com>
2021-10-01 00:01:40 +0530

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,3 +1,5 @@
+/* Write a program to create a class named shape. It should contain 2 methods,
+draw() and erase() that prints “Drawing Shape” and “Erasing Shape” respectively*/
 public class Shape
 {
     public static class Main
